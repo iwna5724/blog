52221454b2af4b8f8d055e6c53053c1360412012
iwna5724 <yc74105@gmail.com>
2026-01-05 01:26:50 +0900

    모바일 버전(APK)을 만들기 위한 사전 작업2

--- static/js/pwa.js
+++ static/js/pwa.js
@@ -5,7 +5,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/static/service-worker.js')
+      .register('./service-worker.js')
       .then((registration) => {
         console.log('[PWA] Service Worker registered:', registration.scope);
         
